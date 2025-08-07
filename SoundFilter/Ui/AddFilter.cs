7b1aa5495195a3cd91d057f9fcd5de38cd9e2693
Robert Baker <reiichi001@gmail.com>
2025-08-06 20:26:41 -0700

    Start API 13
    Crashes because ResourceHandler needs to be updated.

--- SoundFilter/Ui/AddFilter.cs
+++ SoundFilter/Ui/AddFilter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Dalamud.Interface;
-using ImGuiNET;
+using Dalamud.Bindings.ImGui;
 using SoundFilter.Config;
 using SoundFilter.Resources;
 
--- SoundFilter/Ui/Settings.cs
+++ SoundFilter/Ui/Settings.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Numerics;
 using Dalamud.Interface;
-using ImGuiNET;
+using Dalamud.Bindings.ImGui;
 using SoundFilter.Resources;
 
 namespace SoundFilter.Ui;
